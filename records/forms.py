1a1c2f475510b7ff28ff73c597b625fe2f2ac8cd
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2025-05-29 13:16:44 +0500

    Add: form for subcategory

--- records/forms.py
+++ records/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from records.models import Record
+from records.models import Record, SubCategory
 
 
 class RecordForm(forms.ModelForm):
     class Meta:
         model = Record
         fields = ['purpose', 'type', 'category', 'subcategory', 'sum', 'note']
+
+
+class SubCategoryForm(forms.ModelForm):
+    class Meta:
+        model = SubCategory
+        fields = ['name']

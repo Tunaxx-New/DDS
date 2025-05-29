5800385157093943a32c6b09bc1ce6a21bcf4930
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2025-05-29 12:25:46 +0500

    Add: urls for CRUD routes

--- records/urls.py
+++ records/urls.py
@@ -6,11 +6,30 @@ from records.views import *
 
 urlpatterns = [
     path('purposes/', RecordPurposeListView.as_view(), name='purposes'),
+    path('purposes/create', RecordPurposeCreateView.as_view(), name='purpose-create'),
+    path('purposes/delete/<int:pk>/', RecordPurposeDeleteView.as_view(), name='purpose-delete'),
+    path('purposes/update/<int:pk>/', RecordPurposeUpdateView.as_view(), name='purpose-update'),
+
     path('types/', RecordTypeListView.as_view(), name='types'),
+    path('types/create', RecordTypeCreateView.as_view(), name='type-create'),
+    path('types/delete/<int:pk>/', RecordTypeDeleteView.as_view(), name='type-delete'),
+    path('types/update/<int:pk>/', RecordTypeUpdateView.as_view(), name='type-update'),
+
     path('categories/', RecordCategoryListView.as_view(), name='categories'),
-    path('subcategories/', RecordSubCategoryListView.as_view(), name='subcategories'),
-    path('records/create/', RecordCreateView.as_view(), name='records-create'),
+    path('categories/create', RecordCategoryCreateView.as_view(), name='category-create'),
+    path('categories/delete/<int:pk>/', RecordCategoryDeleteView.as_view(), name='category-delete'),
+    path('categories/update/<int:pk>/', RecordCategoryUpdateView.as_view(), name='category-update'),
+
+    path('subcategories/<int:category_id>', RecordSubCategoryListView.as_view(), name='subcategories'),
+    path('subcategories/create/<int:category_id>', RecordSubCategoryCreateView.as_view(), name='subcategory-create'),
+    path('subcategories/delete/<int:pk>/', RecordSubCategoryDeleteView.as_view(), name='subcategory-delete'),
+    path('subcategories/update/<int:pk>/', RecordSubCategoryUpdateView.as_view(), name='subcategory-update'),
+    path('ajax/load-subcategories/', load_subcategories, name='ajax_load_subcategories'),
+
     path('', RecordListView.as_view(), name='records'),
+    path('records/create/', RecordCreateView.as_view(), name='records-create'),
+    path('records/delete/<int:pk>/', RecordDeleteView.as_view(), name='records-delete'),
+    path('records/update/<int:pk>/', RecordUpdateView.as_view(), name='records-update'),
 ]
 
 urlpatterns.extend(
